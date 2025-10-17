675b0e45a7217014d0fc6d9f1ecb0f504544fd1e
Julia Zanotta <julia_zanotta@hotmail.com>
2025-10-17 10:40:57 -0300

    3rd section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import Section1 from './components/Section1'
 import Section2 from './components/Section2'
+import Section3 from './components/Section3'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Section1 />
       <Section2 />
+      <Section3 />
     </>
   )
 }
---
+++ src/components/Section3.jsx
@@ -0,0 +1,37 @@
+import './Section3.css'
+
+export default function Section3() {
+    return (
+        <section class="section3">
+            <h1>um app pensado para você</h1>
+
+            <div class="container">
+                <div class="texto">
+                    <h2>explore um closet infinito</h2>
+                    <p>navegue por um feed com novidades constantes e encontre exatamente o que você procura. <br />
+                        filtre por categoria, cor, tamanho e encontre peças incríveis na sua cidade.</p>
+                </div>
+                <img src="./s3m1.png" alt="" />
+            </div>
+
+            <div class="container_dif">
+                <img src="./s3m2.png" alt="" />
+                <div class="texto_dif">
+                    <h2>todos os detalhes na sua mão</h2>
+                    <p>veja fotos reais, descrições detalhadas, a localização da peça e a disponibilidade no calendário. <br />
+                        tudo que vc precisa para alugar com confiança.</p>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="texto">
+                    <h2>anuncie suas peças em minutos</h2>
+                    <p>tirar suas peças do armário e transformá-las em renda extra nunca foi tão fácil. <br />
+                    nosso formulário de anúncio é simples e intuitivo.</p>
+                </div>
+                <img src="./s3m3.png" alt="" />
+            </div>
+
+        </section>
+    )
+}
