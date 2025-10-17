9d9c754c086f3b98eeaa3e00cd50f9bd46e38baf
Julia Zanotta <julia_zanotta@hotmail.com>
2025-10-17 11:25:57 -0300

    5th section - unfineshed

---
+++ src/components/Section5.jsx
@@ -0,0 +1,30 @@
+import './Section5.css'
+
+export default function Section5() {
+    return(
+        <section class="section5">
+
+            <div class="title">
+                <h1>pronta para fazer a moda circular?</h1>
+                <h2>save the date <br />
+                dez/2025</h2>
+            </div>
+
+            <div class="container5">
+                <h1>seja para encontrar um look único ou para dar uma vida nova às suas peças, O Closet é a sua comunidade.</h1>
+                <p>esse projeto nasceu da união de duas paixões: moda e tecnologia. e também de um sonho: provar que a moda pode ser mais circular, acessível e divertida. cada detalhe está sendo construído para tirar essa ideia do papel e transformá-la na sua nova plataforma favorita.</p>
+            </div>
+
+            <div class="end">
+                <img src="./s5m1.png" alt="" />
+
+                <div class="zap">
+                    <h2>ficou com alguma dúvida?<br />
+                    manda pra gente!</h2>
+                    <img src="./s5m2.png" alt="" />
+                    <p>clica aqui :)</p>
+                </div>
+            </div>
+        </section>
+    )
+}
