1add5fcc3a45dd7c41c4c5479a6e493bde1a80b1
Julia Zanotta <julia_zanotta@hotmail.com>
2025-10-23 11:52:28 -0300

    commit emergencial

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -5,7 +5,7 @@ export default function Section1() {
         <section class="section1">
             <img src="/icon.png" alt="" />
             <div>
-                <h1>roupas incríveis para momentos únicos. <br />sem o peso no seu armário (nem no seu bolso).</h1>
+                <h1>roupas incríveis para momentos únicos. <br />sem o peso no seu armário <br />(nem no seu bolso).</h1>
                 <h3>a plataforma que conecta pessoas e roupas de forma simples, segura e sustentável.</h3>
                 <a href="#">quero conhecer!</a>
             </div>
