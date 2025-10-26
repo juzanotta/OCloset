11ad8706a5d3c76a2ac72781f6371fdd759bbb78
Julia Zanotta <julia_zanotta@hotmail.com>
2025-10-26 19:08:53 -0300

    ajustes header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ export default function Header() {
     return (
         <header>
             <p>save the date</p>
+            <a href="#">
             <img src="./logo.png" alt="" />
+            </a>
             <p>dez/2025</p>
         </header>
     )
